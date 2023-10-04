d6969807879005dede228c1ff842a6e0f01d19ce
samh241 <sharrison1@companieshouse.gov.uk>
2023-10-04 15:26:21 +0100

    adding data lookup for ami-id

--- groups/alteryx-sandbox/data.tf
+++ groups/alteryx-sandbox/data.tf
@@ -61,7 +61,7 @@ data "aws_s3_bucket" "resources" {
 }
 
 data "aws_ami" "alteryx_server_ami" {
-  count = var.backend_ami_id == "" ? 1 : 0
+  count = var.alteryx_server_ami_id == "" ? 1 : 0
 
   most_recent = true
   name_regex  = "^alteryx-server-ami-${var.alteryx_server_ami_version_pattern}$"
@@ -69,7 +69,7 @@ data "aws_ami" "alteryx_server_ami" {
 }
 
 data "aws_ami" "alteryx_worker_ami" {
-  count = var.backend_ami_id == "" ? 1 : 0
+  count = var.alteryx_worker_ami_id == "" ? 1 : 0
 
   most_recent = true
   name_regex  = "^alteryx-server-ami-${var.alteryx_worker_ami_version_pattern}$"
