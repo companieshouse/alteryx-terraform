16557867c138e831b5ccd684ec2644002b5c43ec
samh241 <sharrison1@companieshouse.gov.uk>
2024-04-09 15:03:08 +0100

    Adding ALB for sandbox server

--- groups/alteryx-sandbox/alb.tf
+++ groups/alteryx-sandbox/alb.tf
@@ -1,13 +1,14 @@
 # Load Balancer
 resource "aws_lb" "alteryx_sandbox" {
-  name               = "${var.environment}-${var.service}"
+  name               = "${var.environment}-${var.application}"
   internal           = true
   load_balancer_type = "application"
   security_groups    = [module.alteryx_server_ec2_security_group.this_security_group_id]
   subnets            = var.application_subnets[*]
+  
   tags = {
     Environment = var.environment
-    Service     = var.service
+    Service     = var.application
   }
 }
 
@@ -18,6 +19,7 @@ resource "aws_lb_target_group" "alteryx_sandbox_web" {
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = data.aws_vpc.vpc.id
+  
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
@@ -30,7 +32,6 @@ resource "aws_lb_target_group" "alteryx_sandbox_web" {
 }
 
 resource "aws_lb_target_group_attachment" "alteryx_sandbox_web" {
-  count            = var.alteryx_server_instance_count
   target_group_arn = aws_lb_target_group.alteryx_sandbox_web.arn
   target_id        = var.alteryx_server_private_ip
   port             = 80
@@ -40,8 +41,8 @@ resource "aws_lb_target_group_attachment" "alteryx_sandbox_web" {
 resource "aws_acm_certificate" "certificate" {
   count = local.create_ssl_certificate ? 1 : 0
 
-  domain_name               = "${var.service}.${var.environment}.${local.dns_zone_name}"
-  subject_alternative_names = ["*.${var.service}.${var.environment}.${local.dns_zone_name}"]
+  domain_name               = "${var.application}.${var.environment}.${local.dns_zone_name}"
+  subject_alternative_names = ["*.${var.application}.${var.environment}.${local.dns_zone_name}"]
   validation_method         = "DNS"
 }
 
@@ -75,7 +76,7 @@ resource "aws_lb_listener" "alteryx_sandbox_listener_443" {
   port              = "443"
   protocol          = "HTTPS"
   certificate_arn   = local.ssl_certificate_arn
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alteryx_sandbox_web.arn
