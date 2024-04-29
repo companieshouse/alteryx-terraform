e98725b84cf5756841b43c2d9b340ddee162a428
samh241 <sharrison1@companieshouse.gov.uk>
2024-04-29 11:23:16 +0100

    Updating load balancer name

--- groups/alteryx-sandbox/security_groups.tf
+++ groups/alteryx-sandbox/security_groups.tf
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Applicatoin Security Group and rules
+# Application Security Group and rules
 # ------------------------------------------------------------------------------
 resource "aws_security_group" "ec2" {
   name        = "sgr-${var.application}-${var.application_environment}-server"
@@ -66,34 +66,34 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_rdpudp" {
 resource "aws_vpc_security_group_ingress_rule" "ec2_http" {
   description                  = "http Access"
   security_group_id            = aws_security_group.ec2.id
-  referenced_security_group_id = aws_security_group.ec2_alb.id
+  referenced_security_group_id = aws_security_group.ec2_nlb.id
   ip_protocol                  = "tcp"
   from_port                    = 80
   to_port                      = 80
 }
 
 # ------------------------------------------------------------------------------
-# ALB Security Group and rules
+# nlb Security Group and rules
 # ------------------------------------------------------------------------------
 
-resource "aws_security_group" "ec2_alb" {
-  name        = "sgr-${var.application}-${var.application_environment}-server-alb"
-  description = "Security group for the ${var.application_environment} ${var.application} Server ALB"
+resource "aws_security_group" "ec2_nlb" {
+  name        = "sgr-${var.application}-${var.application_environment}-server-nlb"
+  description = "Security group for the ${var.application_environment} ${var.application} Server nlb"
   vpc_id      = data.aws_vpc.vpc.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "httpsalb" {
-  description       = "alb Access"
-  security_group_id = aws_security_group.ec2_alb.id
+resource "aws_vpc_security_group_ingress_rule" "httpsnlb" {
+  description       = "nlb Access"
+  security_group_id = aws_security_group.ec2_nlb.id
   prefix_list_id    = data.aws_ec2_managed_prefix_list.admin.id
   ip_protocol       = "tcp"
   from_port         = 443
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_egress_rule" "instancealb" {
+resource "aws_vpc_security_group_egress_rule" "instancenlb" {
   description                  = "HTTP to instance"
-  security_group_id            = aws_security_group.ec2_alb.id
+  security_group_id            = aws_security_group.ec2_nlb.id
   referenced_security_group_id = aws_security_group.ec2.id
   ip_protocol                  = "tcp"
   from_port                    = 80
