051dd7c75b171577c899541b45b627472fc3766b
samh241 <sharrison1@companieshouse.gov.uk>
2024-05-29 12:56:12 +0100

    updating sg

--- groups/alteryx-sandbox/security_groups.tf
+++ groups/alteryx-sandbox/security_groups.tf
@@ -36,6 +36,17 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_wmi_azure" {
   cidr_ipv4         = each.value
 }
 
+resource "aws_vpc_security_group_ingress_rule" "ec2_wmi_azure_dcom" {
+  for_each = toset(local.azure_dc_cidrs)
+
+  description       = "WMI Access DCOM range"
+  security_group_id = aws_security_group.ec2.id
+  ip_protocol       = "tcp"
+  from_port         = 49152
+  to_port           = 65535
+  cidr_ipv4         = each.value
+}
+
 resource "aws_vpc_security_group_ingress_rule" "ec2_ansible" {
   description       = "Ansible Access"
   security_group_id = aws_security_group.ec2.id
@@ -70,6 +81,15 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_http" {
   ip_protocol                  = "tcp"
   from_port                    = 80
   to_port                      = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_worker_api" {
+  description       = "Worker api access"
+  security_group_id = module.alteryx_worker_ec2_security_group.this_security_group_id
+  prefix_list_id    = data.aws_ec2_managed_prefix_list.ci.id
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
 }
 
 # ------------------------------------------------------------------------------
