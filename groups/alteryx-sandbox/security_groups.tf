e866e166b878a415c5ccc62aa95e779e9cffb699
samh241 <sharrison1@companieshouse.gov.uk>
2024-05-31 09:17:36 +0100

    adding egress rule and removing old server sg

--- groups/alteryx-sandbox/security_groups.tf
+++ groups/alteryx-sandbox/security_groups.tf
@@ -97,7 +97,7 @@ resource "aws_vpc_security_group_egress_rule" "ec2_egress" {
   security_group_id = aws_security_group.ec2.id
 
   cidr_ipv4   = "0.0.0.0/0"
-  ip_protocol = "All"
+  ip_protocol = "-1"
 }
 
 # ------------------------------------------------------------------------------
