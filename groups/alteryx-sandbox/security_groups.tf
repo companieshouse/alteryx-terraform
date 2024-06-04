33f864c279e0a7ed49d3ce1002321c1429d656a2
samh241 <sharrison1@companieshouse.gov.uk>
2024-06-04 14:41:16 +0100

    adding new sg for sandbox worker

--- groups/alteryx-sandbox/security_groups.tf
+++ groups/alteryx-sandbox/security_groups.tf
@@ -36,6 +36,17 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_wmi_azure" {
   cidr_ipv4         = each.value
 }
 
+resource "aws_vpc_security_group_ingress_rule" "ec2_wmi_azure_dcom" {
+  for_each = toset(local.azure_dc_cidrs)
+
+  description       = "WMI Access DCOM range"
+  security_group_id = aws_security_group.ec2.id
+  ip_protocol       = "tcp"
+  from_port         = 49152
+  to_port           = 65535
+  cidr_ipv4         = each.value
+}
+
 resource "aws_vpc_security_group_ingress_rule" "ec2_ansible" {
   description       = "Ansible Access"
   security_group_id = aws_security_group.ec2.id
@@ -70,6 +81,70 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_http" {
   ip_protocol                  = "tcp"
   from_port                    = 80
   to_port                      = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_worker_api" {
+  description                  = "Worker api access"
+  security_group_id            = aws_security_group.ec2.id
+  referenced_security_group_id = module.alteryx_worker_ec2_security_group.this_security_group_id
+  ip_protocol                  = "tcp"
+  from_port                    = 80
+  to_port                      = 80
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "ec2_egress" {
+  security_group_id = aws_security_group.ec2.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+# ------------------------------------------------------------------------------
+# Worker Security Group and rules
+# ------------------------------------------------------------------------------
+
+resource "aws_security_group" "ec2_worker" {
+  name        = "sgr-${var.application}-${var.application_environment}-worker"
+  description = "Security group for the ${var.application_environment} ${var.application} Worker EC2"
+  vpc_id      = data.aws_vpc.vpc.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_worker_wmi_azure" {
+  for_each = toset(local.azure_dc_cidrs)
+
+  description       = "WMI Access"
+  security_group_id = aws_security_group.ec2_worker.id
+  ip_protocol       = "tcp"
+  from_port         = 135
+  to_port           = 135
+  cidr_ipv4         = each.value
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_worker_wmi_azure_dcom" {
+  for_each = toset(local.azure_dc_cidrs)
+
+  description       = "WMI Access DCOM range"
+  security_group_id = aws_security_group.ec2_worker.id
+  ip_protocol       = "tcp"
+  from_port         = 49152
+  to_port           = 65535
+  cidr_ipv4         = each.value
+}
+resource "aws_vpc_security_group_ingress_rule" "ec2_worker_ansible" {
+  description       = "Ansible Access"
+  security_group_id = aws_security_group.ec2_worker.id
+  prefix_list_id    = data.aws_ec2_managed_prefix_list.ci.id
+  ip_protocol       = "tcp"
+  from_port         = 5986
+  to_port           = 5986
+}
+
+resource "aws_vpc_security_group_egress_rule" "ec2_worker_egress" {
+  security_group_id = aws_security_group.ec2_worker.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
 }
 
 # ------------------------------------------------------------------------------
