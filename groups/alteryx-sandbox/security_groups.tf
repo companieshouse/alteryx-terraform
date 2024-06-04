944b73382df803fa767d6aef1af6b288723c13c5
samh241 <sharrison1@companieshouse.gov.uk>
2024-06-04 14:39:59 +0100

    adding new sg for sandbox worker

--- groups/alteryx-sandbox/security_groups.tf
+++ groups/alteryx-sandbox/security_groups.tf
@@ -101,6 +101,53 @@ resource "aws_vpc_security_group_egress_rule" "ec2_egress" {
 }
 
 # ------------------------------------------------------------------------------
+# Worker Security Group and rules
+# ------------------------------------------------------------------------------
+
+resource "aws_security_group" "ec2_worker" {
+  name        = "sgr-${var.application}-${var.application_environment}-worker"
+  description = "Security group for the ${var.application_environment} ${var.application} Worker EC2"
+  vpc_id      = data.aws_vpc.vpc.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_worker_wmi_azure" {
+  for_each = toset(local.azure_dc_cidrs)
+
+  description       = "WMI Access"
+  security_group_id = aws_security_group.ec2_worker.id
+  ip_protocol       = "tcp"
+  from_port         = 135
+  to_port           = 135
+  cidr_ipv4         = each.value
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_worker_wmi_azure_dcom" {
+  for_each = toset(local.azure_dc_cidrs)
+
+  description       = "WMI Access DCOM range"
+  security_group_id = aws_security_group.ec2_worker.id
+  ip_protocol       = "tcp"
+  from_port         = 49152
+  to_port           = 65535
+  cidr_ipv4         = each.value
+}
+resource "aws_vpc_security_group_ingress_rule" "ec2_worker_ansible" {
+  description       = "Ansible Access"
+  security_group_id = aws_security_group.ec2_worker.id
+  prefix_list_id    = data.aws_ec2_managed_prefix_list.ci.id
+  ip_protocol       = "tcp"
+  from_port         = 5986
+  to_port           = 5986
+}
+
+resource "aws_vpc_security_group_egress_rule" "ec2_worker_egress" {
+  security_group_id = aws_security_group.ec2_worker.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+# ------------------------------------------------------------------------------
 # NLB Security Group and rules
 # ------------------------------------------------------------------------------
 
