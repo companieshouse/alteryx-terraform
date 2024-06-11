a8845d42d50fc9d0c9fdcad85be4a69606997235
samh241 <sharrison1@companieshouse.gov.uk>
2024-06-11 14:37:18 +0100

    Updating live providers

--- groups/alteryx-live/main.tf
+++ groups/alteryx-live/main.tf
@@ -3,12 +3,12 @@
 # Providers
 # ------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 0.13.0, < 0.14"
+  required_version = ">= 0.13.0, < 2.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 0.3, < 4.0"
+      version = ">= 0.3, < 6.0"
     }
     vault = {
       source  = "hashicorp/vault"
